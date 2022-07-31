5da21b5e55f4fb880e5828925798d76701851aff
Yakov Litvin <Yakov.A.Litvin@gmail.com>
2022-07-31 16:56:08 +0300

    update latest tested TW version to 2.9.3

--- index.php
+++ index.php
@@ -634,7 +634,7 @@ function getTwVersion($wikiFileText) {
 	return $match;
 }
 define("EARLIEST_TESTED_VERSION", 20600);
-define("LATEST_TESTED_VERSION", 20902);
+define("LATEST_TESTED_VERSION", 20903);
 function isSupportedTwVersion($versionParts) {
 
 	if(!$versionParts)
