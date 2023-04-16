dc44c40e762904ae6a41cd9112d61cbb3fe92a4c
Yakov Litvin <Yakov.A.Litvin@gmail.com>
2023-04-16 10:39:22 +0300

    ui: make inputs' background color follow the theme

--- index.php
+++ index.php
@@ -802,7 +802,7 @@ function showMtsPage($html, $title = '', $httpStatus = 200) {
 				}
 
 				input, select, textarea { font-family: inherit; font-size: inherit; padding-left: 0.2em; }
-				select { background: inherit; color: inherit; }
+				input, textarea, select { background: inherit; color: inherit; }
 				option { background: var(--color-background); }
 				/* the hover and selected ones are more complecated, see https://stackoverflow.com/q/10484053/3995261 and https://stackoverflow.com/q/8619406/3995261 */
 				input[type="text"] {  } /* keep disabled in mind */
